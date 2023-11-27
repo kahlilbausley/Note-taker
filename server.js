2420debe98e2ca2a71f9f8839bb50cdf1b40b3eb
kahlilbausley <42008951+kahlilbausley@users.noreply.github.com>
2023-11-27 12:33:52 -0800

    deploye to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var jsonServer = require('json-server');
 var server = express();
 server.use(express.static("public"))
+const PORT = process.env.PORT || 3000
 // ...
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
@@ -9,4 +10,4 @@ server.use(express.static("public"))
 const router = jsonServer.router('db.json');
 router.db._.id = "title";
 server.use("/api", router); 
-server.listen(3000);
+server.listen(PORT);
